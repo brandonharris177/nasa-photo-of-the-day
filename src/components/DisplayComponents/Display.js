96d191d710ac55b6c01dbb0e0c96b0f1ac47f7ec
Brandon-Harris <brandonharris177@gmail.com>
2019-08-14 13:40:53 -0600

    file structure

---
+++ src/components/DisplayComponents/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// const Display = (props) => {
+//   console.log("Display", props, props.number);
+//   return <div>{/* Display any props data here */}
+//   {props.number}
+//   {props.operator}
+//   {props.special}
+//   </div>;
+// };
+
+export default Display;
