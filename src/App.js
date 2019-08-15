538a2b8699a18314f61f3f609868d9490a945b99
Brandon-Harris <brandonharris177@gmail.com>
2019-08-15 14:42:48 -0600

    met mvp

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import Title from "./components/component-parts/goGetTitle";
 import Date from "./components/component-parts/goGetDate"
 import styled from "styled-components";
 import 'semantic-ui-css/semantic.min.css'
+import MenuExampleBasic from "./components/Menu"
+
+const SubscribeButton = styled.button`
+  color: white;
+  padding: 20px;
+  border radius: 10%;
+  background-color: blue;
+  `;
 
 
 function App() {
   return (
     <div className = "App">
-      <div className = "header">
+      <div className = "menu">
+        <MenuExampleBasic/>
         <Title/>
       </div>
       <div className = "image-container">
         <Display/>
       </div>
       <div>
-      <div className = "blank">
-      </div>
+        <SubscribeButton>Subscribe</SubscribeButton>
         <Date/>
       </div>
     </div>
